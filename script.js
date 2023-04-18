7895a43a8314127a253de6e682b564b09a51506a
Logon Aluno <logonrmlocal@fiap.com.br>
2023-04-18 12:11:20 -0300

    first commit

--- script.js
+++ script.js
@@ -14,26 +14,26 @@ function validar(){
 function calc(){
     let n1 = Number(prompt("Digite o primeiro valor"))
     let n2 = Number(prompt("Digite o segundo valor"))
-    let op = Number(prompt('DADOS INFORMADOS: ${n1} e ${n2}. \nESCOLHA UMA OPÇÃO: \n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4] Dividir'))
-    let msg = documet.getElementById('msg')
-    msg.innetHTML = '<h2>Processando o resultado</h2>'
+    let op = Number(prompt(`DADOS INFORMADOS: ${n1} e ${n2}. \nESCOLHA UMA OPÇÃO: \n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4] Dividir`))
+    let msg = document.getElementById('msg')
+    msg.innetHTML = `<h2>Processando o resultado</h2>`
     
     switch(op){
 
         case 1:
-            msg.innerHTML +='<p>${n1} + ${n2} = <strong>${n1+n2}</strong></p>'
+            msg.innerHTML +=`<p>${n1} + ${n2} = <strong>${n1+n2}</strong></p>`
             break
         case 2:
-            msg.innerHTML +='<p>${n1} - ${n2} = <strong>${n1-n2}</strong></p>'
+            msg.innerHTML +=`<p>${n1} - ${n2} = <strong>${n1-n2}</strong></p>`
             break
          case 3:
-            msg.innerHTML +='<p>${n1} * ${n2} = <strong>${n1*n2}</strong></p>'
+            msg.innerHTML +=`<p>${n1} * ${n2} = <strong>${n1*n2}</strong></p>`
             break    
         case 4:
-            msg.innerHTML +='<p>${n1} / ${n2} = <strong>${n1/n2}</strong></p>'
+            msg.innerHTML +=`<p>${n1} / ${n2} = <strong>${n1/n2}</strong></p>`
             break
         default:
-            msg.innerHTML +='<p>OPÇÃO INVÁLIDA</p>'
+            msg.innerHTML +=`<p>OPÇÃO INVÁLIDA</p>`
             break
     }
 }
